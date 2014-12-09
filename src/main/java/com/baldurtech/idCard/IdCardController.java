54f789fb6dd66724e71663e5225336dc02e7bc90
baijiayong <jiayongbai@gmail.com>
2014-12-09 12:14:31 +0800

    在controller中添加了list和save方法，并且添加了list页面

--- src/main/java/com/baldurtech/idCard/IdCardController.java
+++ src/main/java/com/baldurtech/idCard/IdCardController.java
@@ -1,8 +1,11 @@
 package com.baldurtech.idCard;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 @RequestMapping("idCard")
@@ -10,5 +13,25 @@ public class IdCardController {
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String add() {
         return "idCard/add";
+    }
+    
+    @RequestMapping(value = "list", method = RequestMethod.GET) 
+    public String list() {
+        return "idCard/list";
+    }
+    
+    @RequestMapping(value = "save", method = RequestMethod.POST)
+    public String save(@ModelAttribute("idCard") IdCard idCard,
+                       @RequestParam("photo") MultipartFile photo) {
+       System.out.println(idCard.getName());
+       System.out.println(idCard.getFolk());
+       System.out.println(idCard.getGender());
+       System.out.println(idCard.getBirthday());
+       System.out.println(idCard.getAddress());
+       System.out.println(idCard.getAgency());
+       System.out.println(idCard.getContent());
+       System.out.println(idCard.getContentType());
+       System.out.println(photo);
+       return "redirect:list";
     }
 }
