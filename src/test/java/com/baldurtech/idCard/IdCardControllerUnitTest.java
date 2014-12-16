d7edeac4795670a578fce8954faffedbdf88a748
baijiayong <jiayongbai@gmail.com>
2014-12-16 11:24:47 +0800

    添加了controller中list方法的单元测试

---
+++ src/test/java/com/baldurtech/idCard/IdCardControllerUnitTest.java
@@ -0,0 +1,23 @@
+package com.baldurtech.idCard;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+public class IdCardControllerUnitTest {
+    @InjectMocks
+    IdCardController idCardController;
+    
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+    
+    @Test
+    public void 调用list方法时应该返回idCard_list字符串() {
+        assertEquals("idCard/list", idCardController.list());
+    }
+}
