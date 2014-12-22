58573b296231e1dbf7f632206f04e740e64f7c6a
shihang <study1228.sh@gmail.com>
2014-12-22 14:44:22 +0800

    完成了显示event列表的功能

--- src/main/java/com/baldurtech/event/Event.java
+++ src/main/java/com/baldurtech/event/Event.java
@@ -1,11 +1,27 @@
 package com.baldurtech.event;
 
-import java.sql.Date;
+import java.util.Date;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "event")
+@NamedQuery(name = Event.FIND_ALL, query = "from Event")
 public class Event {
+    public static final String FIND_ALL = "Event.findAll";
+    
+    @Id
+    @GeneratedValue
     private Long id;
     private String name;
+    
+    @Temporal( value = TemporalType.TIMESTAMP )
     private Date dateCreated;
+    
+    @PrePersist
+    protected void onCreate() {
+        dateCreated = new Date();
+    }
     
     public Long getId() {
 		return id;
--- src/main/java/com/baldurtech/event/EventRepository.java
+++ src/main/java/com/baldurtech/event/EventRepository.java
@@ -3,26 +3,17 @@ package com.baldurtech.event;
 import java.sql.Date;
 import java.util.List;
 import java.util.ArrayList;
+
+import javax.persistence.*;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class EventRepository {
+    @PersistenceContext
+    EntityManager entityManager;
+    
     public List<Event> findAll() {
-        List<Event> eventList = new ArrayList<Event>();
-        
-        Event event1 = new Event();
-        
-        event1.setDateCreated(Date.valueOf("2007-01-12"));
-        event1.setName("Safety education sign for second Team");
-        
-        Event event2= new Event();
-        
-        event2.setDateCreated(Date.valueOf("2007-05-12"));
-        event2.setName("Safety education sign for first Team");
-        
-        eventList.add(event1);
-        eventList.add(event2);
-        
-        return eventList;
+        return entityManager.createNamedQuery(Event.FIND_ALL, Event.class).getResultList();
     }
 }
